9baca810d01e9adad02e40c502ad9dd6d4c35653
Lida-Ghaemi <74499752+Lida-Ghaemi@users.noreply.github.com>
2020-12-09 12:55:09 -0800

    Add files via upload

---
+++ data/func.cc
@@ -0,0 +1,17 @@
+int div(int x, int y) {
+    float z = 45;
+    if (y != 0) {
+        return x /y;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    auto e = 8;
+    auto r = div(5, e);
+    auto pr = &r;
+    *pr = 45;
+    int& ref = r;
+    return r;
+}
